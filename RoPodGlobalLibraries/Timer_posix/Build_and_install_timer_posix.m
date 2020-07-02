26eeefad0010587c255573857fb35fefd357b4d4
AJvanDommelen <a.j.v.dommelen@student.tue.nl>
2020-07-02 11:54:09 +0200

    Removed previously updated file

--- RoPodGlobalLibraries/Timer_posix/Build_and_install_timer_posix.m
+++
@@ -1,8 +0,0 @@
-disp('%%%%%%%%%%%%% Installing Timer_posix %%%%%%%%%%%%%%%%')
-%% Timer_posix
-
-disp('> building libtimer_posix...')
-!sudo chmod a+x buildlib
-!sudo ./buildlib
-
-disp('installed')
